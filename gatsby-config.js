55c577ef7c8c4f1704ce012dd1641a3a3df0fd1c
Damiao02 <55301337+Damiao02@users.noreply.github.com>
2019-09-20 17:52:16 -0400

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,12 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [{
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `markdown-pages`,
+      path: `${__dirname}/src/receitas`,
+    },
+  },
+  'gatsby-transformer-remark']
 }
