d437883c5d7c9eb568fa9d3a666b93f9199c0efd
Damiao02 <55301337+Damiao02@users.noreply.github.com>
2019-09-29 23:48:46 -0400

    Add files via upload

---
+++ src/components/pizza-cards.js
@@ -0,0 +1,106 @@
+import React from "react"
+import PizzaCardsStyle from "./pizza-cards.module.css"
+import {Link} from "gatsby"
+import marguerita from '../img/pizza-marguerita.jpg'
+import frango from '../img/pizza-frango.jpg'
+import presunto_queijo from '../img/pizza-presunto-queijo.jpg'
+import serve from '../img/tray.png'
+import tempo from '../img/timer.png'
+
+
+
+export default class PizzaCards extends 
+React.Component {
+    
+ 
+  render() {      
+      return(
+        <main className={PizzaCardsStyle.servicos}>
+              <article className={PizzaCardsStyle.servico}>
+                  <Link to="#"><img src={marguerita} alt="Pizza Marguerita" /></Link>
+                  
+                  <div className={PizzaCardsStyle.inner}>
+                  <Link to="#" style={{fontFamily: "Poppins Regular", color: this.props.color}}>Pizza Marguerita</Link>
+                  <h4 style={{fontFamily: "Poppins Regular", color: this.props.color}}>Receita de Pizza</h4>
+                  <p style={{fontFamily: "Poppins Regular", color: this.props.color}}>Passo a passo para fazer uma pizza de marguerita perfeita 
+                com pequenos e surpreendentes segredos que fazem toda a diferença!</p>
+
+                 <div className={PizzaCardsStyle.border} />
+
+                  <div  className={PizzaCardsStyle.testinfo}>
+                                 <img src={serve} alt="icone serve" />
+                                    <div className={PizzaCardsStyle.testpic} style={{fontFamily: "Poppins Regular", color: this.props.color}}>
+                                            <span>Serve:</span>
+                                            <br/>6 porções
+                                    </div>
+
+                                    <img src={tempo} alt="icone tempo" />
+                                    <div  style={{fontFamily: "Poppins Regular", color: this.props.color}}>
+                                            <span>Tempo:</span>
+                                            <br/>30 minutos
+                                    </div>
+                        </div>
+                 
+                  </div>
+
+              </article>
+
+              <article className={PizzaCardsStyle.servico}>
+                  <Link to="#"><img src={presunto_queijo} alt="Pizza Presunto e Queijo" /></Link>
+                  
+                  <div className={PizzaCardsStyle.inner}>
+                  <Link to="#" style={{fontFamily: "Poppins Regular", color: this.props.color}}>Pizza Presunto e Queijo</Link>
+                  <h4 style={{fontFamily: "Poppins Regular", color: this.props.color}}>Receita de Pizza</h4>
+                  <p style={{fontFamily: "Poppins Regular", color: this.props.color}}>Passo a passo para fazer uma pizza de presunto e queijo perfeita 
+                com pequenos e surpreendentes segredos que fazem toda a diferença!</p>
+
+                    <div className={PizzaCardsStyle.border} />
+
+                <div  className={PizzaCardsStyle.testinfo}>
+                                 <img src={serve} alt="icone serve" />
+                                    <div className={PizzaCardsStyle.testpic} style={{fontFamily: "Poppins Regular", color: this.props.color}}>
+                                            <span>Serve:</span>
+                                            <br/>6 porções
+                                    </div>
+
+                                    <img src={tempo} alt="icone tempo" />
+                                    <div  style={{fontFamily: "Poppins Regular", color: this.props.color}}>
+                                            <span>Tempo:</span>
+                                            <br/>30 minutos
+                                    </div>
+                        </div>
+                  </div>
+              </article>
+
+              <article className={PizzaCardsStyle.servico}>
+                  <Link to="#"><img src={frango} alt="Pizza Frango" /></Link>
+                  
+                  <div className={PizzaCardsStyle.inner}>
+                  <Link to="#" style={{fontFamily: "Poppins Regular", color: this.props.color}}>Pizza Frango</Link>
+                  <h4 style={{fontFamily: "Poppins Regular", color: this.props.color}}>Receita de Pizza</h4>
+                  <p style={{fontFamily: "Poppins Regular", color: this.props.color}}>Passo a passo para fazer uma pizza de frango perfeita 
+                com pequenos e surpreendentes segredos que fazem toda a diferença!</p>
+
+                <div className={PizzaCardsStyle.border} />
+
+                <div  className={PizzaCardsStyle.testinfo}>
+                                 <img src={serve} alt="icone serve" />
+                                    <div className={PizzaCardsStyle.testpic} style={{fontFamily: "Poppins Regular", color: this.props.color}}>
+                                            <span>Serve:</span>
+                                            <br/>6 porções
+                                    </div>
+
+                                    <img src={tempo} alt="icone tempo" />
+                                    <div  style={{fontFamily: "Poppins Regular", color: this.props.color}}>
+                                            <span>Tempo:</span>
+                                            <br/>30 minutos
+                                    </div>
+                        </div>
+                  </div>
+              </article>
+                           
+        </main>
+        
+      );
+  }
+}
