c96dde7b6ee8423882b5ca3129952879a9b519ca
Damiao02 <55301337+Damiao02@users.noreply.github.com>
2019-10-06 14:34:54 -0300

    Estilizados as receitas do markdown

--- src/templates/recipeTemplate.js
+++ src/templates/recipeTemplate.js
@@ -1,24 +1,64 @@
 import React from "react"
 import { graphql } from "gatsby"
+import {Link} from "gatsby"
+import PizzaHeaderReceitas from "../components/pizza-header-receitas"
+import PizzaHeaderCategoria from "../components/pizza-header-categoria"
+
+
+
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
+
   return (
-    <div className="recipe-container">
-      <div className="recipe-data">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="recipe-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+         
+   <div>
+     <PizzaHeaderReceitas/>
+      
+     <PizzaHeaderCategoria/>
+
+    <div className="receitas">
+      <div className="receita">
+         <div className="inner">    
+          <Link to="#">{frontmatter.title}</Link>
+          <div  className="testinfo">
+                                 <img src="https://github.com/Damiao02/PWA-PIZZA/blob/master/src/img/tray.png?raw=true" alt="icone serve" />
+                                    <div className="testpic" >
+                                            <span>Serve:</span>
+                                            <br/>{frontmatter.porcao}
+                                    </div>
+
+                                    <img src="https://github.com/Damiao02/PWA-PIZZA/blob/master/src/img/timer.png?raw=true" alt="icone tempo" />
+                                    <div>
+                                            <span>Tempo:</span>
+                                            <br/>{frontmatter.tempo}
+                                    </div>
+                        </div>  
+          </div> 
+          <Link to="#"><img src={frontmatter.cover} alt="Pizza Marguerita" /></Link>
+
+              <div className="inner">
+
+          <div className="recipe-content"
+          dangerouslySetInnerHTML={{ __html: html}}/>
+
+      </div> 
+          
       </div>
     </div>
+
+   
+
+</div>
+    
+
   )
 }
+
+
 
 export const pageQuery = graphql`
   query($path: String!) {
@@ -28,6 +68,9 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         path
         title
+        porcao
+        tempo
+        cover
       }
     }
   }
